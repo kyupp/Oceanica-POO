b7b5f94f4ff1fb52d540970a5142d089ac4a7ecb
kyupp <gabriel.perez.pacheco@outlook.es>
2025-11-01 13:17:25 -0600

    Primer commit

---
+++ src/main/java/com/mycompany/oceanica/Oceanica.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.oceanica;
+
+/**
+ *
+ * @author kyup
+ */
+public class Oceanica {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
